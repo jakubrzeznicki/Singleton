82ed38e33791e0c6379b4d85bc753cba5aa6609b
Jakub Rzeźnicki <kubakensi@gmail.com>
2018-10-25 18:26:55 +0200

    Create Maven

---
+++ ProjectSingletonMaven/src/main/java/pl/lodz/uni/math/kuba/projectsingletonmaven/Singleton.java
@@ -0,0 +1,25 @@
+package pl.lodz.uni.math.kuba.projectsingletonmaven;
+
+public class Singleton {
+    private static Singleton mySingleton;
+
+    private Singleton() {
+
+    }
+    public static Singleton getInstance(){
+        if(mySingleton == null){
+            mySingleton = new Singleton();
+        }
+        return mySingleton;
+    }
+
+    public void getSomeThing(){
+
+        System.out.println("I am here....");
+    }
+
+    public static void main(String a[]){
+        Singleton st = Singleton.getInstance();
+        st.getSomeThing();
+    }
+}
