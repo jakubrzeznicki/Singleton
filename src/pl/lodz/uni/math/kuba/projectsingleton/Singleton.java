8e67d274d2736988980a9bf75cfe9de41c132fa0
Jakub Rzeźnicki <kubakensi@gmail.com>
2018-10-25 17:50:13 +0200

    Initial commit

---
+++ src/pl/lodz/uni/math/kuba/projectsingleton/Singleton.java
@@ -0,0 +1,24 @@
+package pl.lodz.uni.math.kuba.projectsingleton;
+
+public class Singleton {
+    private static Singleton mySingleton;
+
+    private Singleton() {
+
+    }
+    public static Singleton getInstance(){
+        if(mySingleton == null){
+            mySingleton = new Singleton();
+        }
+        return mySingleton;
+    }
+
+    public void getSomeThing(){
+        System.out.println("I am here....");
+    }
+
+    public static void main(String a[]){
+        Singleton st = Singleton.getInstance();
+        st.getSomeThing();
+    }
+}
